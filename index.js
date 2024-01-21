592fbb5b0352c00f9b5dffeb491317881a4c4f80
Prananda <prananda1996@gmail.com>
2024-01-21 18:20:14 +0700

    Tugas Pertama

---
+++ index.js
@@ -0,0 +1,11 @@
+var nama = 'Prananda'
+var nilai = 70
+var absen = 3
+
+// your code here
+
+if (nilai >= 70 && absen < 5) {
+    console.log(nama + " lulus")
+} else {
+    console.log(nama + " tidak lulus")
+}
